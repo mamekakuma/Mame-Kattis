96ce1f0ec18e9ad855643b5ef7298d0b23032e34
KaKuMa <96521227+mamekakuma@users.noreply.github.com>
2022-08-27 03:08:37 +0300

    Update CodetoSaveLives.py

--- CodetoSaveLives.py
+++ CodetoSaveLives.py
@@ -1,5 +1,4 @@
-len = int( input())
-for i in range(len):
+#Mame Kakuma
     val1 = int( "".join( input().split() ) ) 
     val2 = int( "".join(input().split()  ) ) 
     print( *list( str( val1+val2 ) ) )
