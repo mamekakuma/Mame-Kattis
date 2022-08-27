fedb3754755e75f28ced5ef7ff5b62056820aab8
KaKuMa <96521227+mamekakuma@users.noreply.github.com>
2022-08-27 03:09:33 +0300

    Code to save lives

--- CodetoSaveLives.py
+++ CodetoSaveLives.py
@@ -1,4 +1,4 @@
 #Mame Kakuma
-    val1 = int( "".join( input().split() ) ) 
-    val2 = int( "".join(input().split()  ) ) 
-    print( *list( str( val1+val2 ) ) )
+val1 = int( "".join( input().split() ) ) 
+val2 = int( "".join(input().split()  ) ) 
+print( *list( str( val1+val2 ) ) )
