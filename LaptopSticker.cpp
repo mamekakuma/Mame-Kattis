bbbe9d60a5cc11e3c24efd69d21fc9091ba00e16
KaKuMa <96521227+mamekakuma@users.noreply.github.com>
2022-09-18 05:32:06 +0300

    Laptop Sticker

---
+++ LaptopSticker.cpp
@@ -0,0 +1,11 @@
+//Mame-Kakuma
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int wc , hc , ws , hs ;
+cin >> wc >> hc >> ws >> hs ;
+if (wc > ws+1 and hc > hs+1)
+cout << 1 ;
+else
+cout << 0 ;
+}
